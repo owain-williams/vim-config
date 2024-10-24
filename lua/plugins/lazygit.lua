3fcfc4c921bcf409a07f6b103b2b6566abee54ae
Owain Williams <104087665+owain-williams@users.noreply.github.com>
2024-10-24 11:55:26 +0100

    add telescope lazygit extension

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -12,6 +12,9 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
+	config = function()
+		require("telescope").load_extension("lazygit")
+	end,
 	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
