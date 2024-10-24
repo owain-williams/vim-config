f39dc2e3693f0876eabf9b44d58d24455d2531bd
Owain Williams <104087665+owain-williams@users.noreply.github.com>
2024-10-24 11:37:52 +0100

    update options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,17 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+-- Basic settings
+vim.cmd("set number") -- Enable line numbers
+vim.cmd("set relativenumber") -- Enable relative line numbers
+vim.cmd("set tabstop=2") -- Number of spaces a tab represents
+vim.cmd("set softtabstop=2") -- Number of spaces a tab represents
+vim.cmd("set shiftwidth=2") -- Number of spaces for each indentation
+vim.cmd("set expandtab") -- Convert tabs to spaces
+vim.cmd("set smartindent") -- Automatically indent new lines
+vim.cmd("set cursorline") -- Highlight the current line
+vim.cmd("set termguicolors") -- Enable 24-bit RGB colors
+
+-- Syntax highlighting and filetype plugins
+vim.cmd("syntax enable")
+vim.cmd("filetype plugin indent on")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
